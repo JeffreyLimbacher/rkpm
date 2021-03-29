c1a22d3d4be9b29aa4078538c9f4a118c8fd476e
JeffreyLimbacher <jeffreylimbacher@gmail.com>
2021-03-28 22:48:26 -0700

    Move coordinates to inner dimension

--- rkpm_2d.jl
+++ rkpm_2d.jl
@@ -4,10 +4,10 @@ using VoronoiDelaunay
 
 function get_domain(x_pts)
     np = length(x_pts)
-    x = zeros(np, np, 2)
-    x[:, :, 1] .= x_pts
-    x[:, :, 2] .= x_pts'
-    reshape(x, np * np, 2)
+    x = zeros(2, np, np)
+    x[1, :, :] .= x_pts
+    x[2, :, :] .= x_pts'
+    reshape(x, 2, np * np)
 end
 
 function get_Hx_from_diff_mat(x_x_I, order)
@@ -21,29 +21,25 @@ function get_Hx_from_diff_mat(x_x_I, order)
         end
     end
     Hx_size = Int((order+1)*(order+2)/2)
-    n_eval = size(x_x_I, 1)
-    n_pts = size(x_x_I, 2)
+    n_eval = size(x_x_I, 2)
+    n_pts = size(x_x_I, 3)
     Hx = zeros(Hx_size, n_eval, n_pts)
     Threads.@threads for i=1:n_pts
         for j=1:n_eval
-            @views get_Hx!(x_x_I[i,j,:], order, Hx[:, i, j])
+            @views get_Hx!(x_x_I[:,i,j], order, Hx[:, i, j])
         end
     end
     Hx
 end
 
 function get_diff_matrix(x_I, x)
-    n_pts = size(x, 1)
-    n_eval = size(x_I, 1)
-    x_x_I = zeros(n_eval, n_pts, 2)
-    for i=1:2
-        x_x_I[:,:,i] = X[:,i] .- X_I[:,i]'
-    end
-    x_x_I
+    x_I_r = reshape(X_I,2,1,size(X_I,2))
+    x_I_l = reshape(X_I,2,size(X_I,2),1)
+    x_I_l .- x_I_r
 end
 
 function get_coefficients(x_x_I, Hx_x_I, phi_a, order)
-    n_eval = size(x_x_I, 1)
+    n_eval = size(x_x_I, 2)
     Hx_size = size(Hx_x_I, 1)
     B = zeros(Hx_size, n_eval)
     H0 = zeros(Hx_size)
@@ -58,10 +54,10 @@ function get_coefficients(x_x_I, Hx_x_I, phi_a, order)
 end
 
 function calc_psi(B, Hx_x_I, phi_a)
-    n_eval = size(phi_a, 2)
+    n_eval = size(phi_a, 1)
     n_pts = size(phi_a, 2)
     psi = zeros(n_pts, n_pts)
-    Threads.@threads for i=1:n_pts
+    Threads.@threads for i=1:n_eval
         for j=1:n_pts
             # eq (5.10)
             psi[j,i] = B[:,j]' * Hx_x_I[:, j, i] .* phi_a[j,i]
@@ -91,7 +87,7 @@ function bspline(z)
 end
 
 function basis(x, a)
-    bspline.(sqrt.(sum(x.^2,dims=3))/a)/a
+    dropdims(bspline.(sqrt.(sum(x.^2,dims=1))/a)/a;dims=1)
 end
 
 step_size = .1
@@ -102,7 +98,7 @@ domain_sample = domain[1:1:end]
 f = (x,y) -> cos(x * pi) + sin(y * pi)
 X_I = get_domain(domain_sample)
 n_pts = size(X_I,1)
-Y_I = f.(X_I[:,1], X_I[:,2])
+Y_I = f.(X_I[1,:], X_I[2,:])
 X = get_domain(domain)
 Y = f.(X[:,1], X[:,2])
 n_eval = size(X,1)
@@ -114,9 +110,4 @@ a = 0.3 .* (X_I[end]-X_I[1])
 
 psi = rkpm_shape_funcs(X_I, 3, a)
 u_I = psi * Y_I
-
-
-#surface(X[:,1], X[:,2], u_h[:,1])
 surface(X[:,1], X[:,2], u_I, legend=false)
-#p=scatter(X[:,1], X[:,2], zeros(length(X[:,1])))
-#@show sum(abs.(u_h-Y))/sum(abs.(Y))
